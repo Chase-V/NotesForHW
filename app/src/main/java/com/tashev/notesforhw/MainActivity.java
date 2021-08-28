120eca3bc592aab8622aae2fc67b3277836310d8
Chase-V <38985553+Chase-V@users.noreply.github.com>
2021-08-28 22:06:40 +0300

    Готовое 7 ДЗ.

--- app/src/main/java/com/tashev/notesforhw/MainActivity.java
+++ app/src/main/java/com/tashev/notesforhw/MainActivity.java
@@ -1,8 +1,17 @@
 package com.tashev.notesforhw;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initToolbar();
+        initDrawer(initToolbar());
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.note_container, ListNotesFragment.newInstance())
                 .commit();
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.add, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        Toast.makeText(getApplicationContext(), "Здесь будут настройки если понадобятся", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_about:
+                        Toast.makeText(getApplicationContext(), "NotesForHW v1.0 \nTashev Victor \n2021", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
     }
 }
